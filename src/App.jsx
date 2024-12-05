0d374f262efef4b7ea7c9e4195ed1f2bcca90cc6
Divya <divyareddy0205@gmail.com>
2024-12-05 14:02:42 -0800

    coding practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Counter from './components/Counter';
-import DynamicList from './components/DynamicList';
-import ColorPicker from './components/ColorPicker';
-import PasswordStrengthChecker from './components/PasswordStrengthChecker';
-import CountdownTimerWithAudio from './components/CountdownTimerWithAudio';
-import TextAnimation from './components/TextAnimation';
-import DynamicMovieList from './components/DynamicMovieList';
-import RecipeGenerator from './components/RecipeGenerator';
-import MemeGenerator from './components/MemeGenerator';
-import TextArtGenerator from './components/TextArtGenerator';
-import QuizAppMultipleChoice from './components/QuizAppMultipleChoice';
-import FileExplorer from './components/FileExplorer';
-import Pagination from './components/Pagination';
-import ProgressBar from './components/ProgressBar';
 import Accordion from './components/Accordion';
 
 const App = () => {
@@ -25,21 +11,6 @@ const App = () => {
         
         <Navbar/>
         <Routes>
-            <Route path="/counter" element ={<Counter/>}/>
-            <Route path="/dynamic-list" element={<DynamicList/>}/>
-            <Route path="/color-picker" element={<ColorPicker/>}/>
-            <Route path="/password-strength" element={<PasswordStrengthChecker/>}/>
-            <Route path="/countdown-timer-audio" element={<CountdownTimerWithAudio/>}/>
-            <Route path="/text-animation" element={<TextAnimation/>}/>
-            <Route path="/text-animation" element={<TextAnimation/>}/>
-            <Route path="/dynamic-movies" element={<DynamicMovieList/>}/>
-            <Route path="/receipe-generator" element={<RecipeGenerator/>}/>
-            <Route path="/meme-generator" element={<MemeGenerator/>}/>
-            <Route path="/text-art-generator" element={<TextArtGenerator/>}/>
-            <Route path="/quiz-with-multiple-choice" element={<QuizAppMultipleChoice/>}></Route>
-            <Route path="/file-explorer" element={<FileExplorer/>}></Route>
-            <Route path="/pagination" element={<Pagination/>}/>
-            <Route path="/progressbar" element={<ProgressBar/>}/>
             <Route path="/accordion" element={<Accordion/>}/>
         </Routes>
       </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,49 +6,7 @@ const Navbar = () => {
             <nav className="absolute">
                 <ul className="font-semibold text-gray-500">
                     <li>
-                        <Link to="/counter">1 - Counter</Link>
-                    </li>
-                    <li>
-                        <Link to="/dynamic-list">2 - Dynamic List of Items with Strikethrough</Link>
-                    </li>
-                    <li>
-                        <Link to="/color-picker">3 - Color Picker with Hex Code Display</Link>
-                    </li>
-                    <li>
-                        <Link to="/password-strength">4 - Password Strength Checker</Link>
-                    </li>
-                    <li>
-                        <Link to="/countdown-timer-audio">5 - Countdown Timer with Audio</Link>
-                    </li>
-                    <li>
-                        <Link to="/text-animation">5 - text-animation</Link>
-                    </li>
-                    <li>
-                        <Link to="/dynamic-movies">6 - Dynamic Movie List with Filtering and Sorting</Link>
-                    </li>
-                    <li>
-                        <Link to="/receipe-generator">7 -  Receipe Generator</Link>
-                    </li>
-                    <li>
-                        <Link to="/meme-generator">8 - Meme Generator</Link>
-                    </li>
-                    <li>
-                        <Link to="/text-art-generator">9 - Dynamically Generated Text Art</Link>
-                    </li>
-                    <li>
-                        <Link to="/quiz-with-multiple-choice">10 - Simple Quiz App with Multiple Choice</Link>
-                    </li>
-                    <li>
-                        <Link to="/file-explorer">11 - File Explorer</Link>
-                    </li>
-                    <li>
-                        <Link to="/pagination">12 - Pagination</Link>
-                    </li>
-                    <li>
-                        <Link to="/progressbar">13 - Progress Bar</Link>
-                    </li>
-                    <li>
-                        <Link to="/Accordion">14 - Accordion</Link>
+                        <Link to="/Accordion">1 - Accordion</Link>
                     </li>
                 </ul>
             </nav>
